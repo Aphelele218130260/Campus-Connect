868896f8bb309365cdffde24d1d33df528e91889
MatthewMcGregor03 <matthewmcgregor2003@gmail.com>
2024-10-18 09:49:52 +0200

    StudentRegistration

--- src/components/StudentRegistration.jsx
+++ src/components/StudentRegistration.jsx
@@ -59,7 +59,7 @@ const StudentRegistration = () => {
             <div className="main-content">
                 <div className="registration-container">
                     <form onSubmit={handleSubmit} className="registration-form">
-                        {/* Form Fields */}
+                        {/* First Name */}
                         <div className="input-group">
                             <label>First Name:</label>
                             <input
@@ -70,7 +70,172 @@ const StudentRegistration = () => {
                                 required
                             />
                         </div>
-                        {/* More input fields go here... */}
+                        {/* Middle Name */}
+                        <div className="input-group">
+                            <label>Middle Name:</label>
+                            <input
+                                type="text"
+                                name="middleName"
+                                value={student.middleName}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        {/* Last Name */}
+                        <div className="input-group">
+                            <label>Last Name:</label>
+                            <input
+                                type="text"
+                                name="lastName"
+                                value={student.lastName}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        {/* Email */}
+                        <div className="input-group">
+                            <label>Email:</label>
+                            <input
+                                type="email"
+                                name="email"
+                                value={student.email}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        {/* Username */}
+                        <div className="input-group">
+                            <label>Username:</label>
+                            <input
+                                type="text"
+                                name="username"
+                                value={student.username}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        {/* Password */}
+                        <div className="input-group">
+                            <label>Password:</label>
+                            <input
+                                type="password"
+                                name="password"
+                                value={student.password}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        {/* Academic Institution */}
+                        <div className="input-group">
+                            <label>Academic Institution:</label>
+                            <input
+                                type="text"
+                                name="academicInstitution"
+                                value={student.academicInstitution}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        {/* Academic Year */}
+                        <div className="input-group">
+                            <label>Academic Year:</label>
+                            <input
+                                type="text"
+                                name="academicYear"
+                                value={student.academicYear}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        {/* Student Number */}
+                        <div className="input-group">
+                            <label>Student Number:</label>
+                            <input
+                                type="text"
+                                name="studentNumber"
+                                value={student.studentNumber}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        {/* Address Line 1 */}
+                        <div className="input-group">
+                            <label>Address Line 1:</label>
+                            <input
+                                type="text"
+                                name="addressLine1"
+                                value={student.addressLine1}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        {/* Address Line 2 */}
+                        <div className="input-group">
+                            <label>Address Line 2:</label>
+                            <input
+                                type="text"
+                                name="addressLine2"
+                                value={student.addressLine2}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        {/* City */}
+                        <div className="input-group">
+                            <label>City:</label>
+                            <input
+                                type="text"
+                                name="city"
+                                value={student.city}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        {/* Postal Code */}
+                        <div className="input-group">
+                            <label>Postal Code:</label>
+                            <input
+                                type="text"
+                                name="postalCode"
+                                value={student.postalCode}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        {/* Nationality */}
+                        <div className="input-group">
+                            <label>Nationality:</label>
+                            <input
+                                type="text"
+                                name="nationality"
+                                value={student.nationality}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+                        {/* Race */}
+                        <div className="input-group">
+                            <label>Race:</label>
+                            <input
+                                type="text"
+                                name="race"
+                                value={student.race}
+                                onChange={handleChange}
+                                required
+                            />
+                        </div>
+
+                        {/* Gender Section as a Combobox */}
+                        <div className="input-group">
+                            <label>Gender:</label>
+                            <select
+                                name="gender"
+                                value={student.gender}
+                                onChange={handleChange}
+                                required
+                            >
+                                <option value="male">Male</option>
+                                <option value="female">Female</option>
+                            </select>
+                        </div>
 
                         {/* Buttons */}
                         <div className="buttons">
